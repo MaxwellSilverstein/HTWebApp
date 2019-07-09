60ab8b6e3f4d19215f7a0a107f4ce50a94a53821
Maxwell Silverstein <mrs84@duke.edu>
2019-07-09 13:38:36 +0300

    wow

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 $('#upload').click(function(){
 
 
-$('#getDataSet').html("<b>penis</b>");
+$('#getDataSet').html("<b>wow</b>");
 
 });
+
+var getDat = document.getElementById('getDataSet');
+getDat.onclick = function(){
+$('#upload').html("<b>wow</b>");
+
+}
